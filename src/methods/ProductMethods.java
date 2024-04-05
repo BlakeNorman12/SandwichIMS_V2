3683d69908d9849baccdbb3ea1b327632185350c
BlakeNorman12 <bnorman718@outlook.com>
2024-04-04 19:56:10 -0500

    Added empty product methods

---
+++ src/methods/ProductMethods.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package methods;
+
+/**
+ *
+ * @author bnorm
+ */
+public class ProductMethods {
+    
+    public static void addProduct(){};
+    
+    public static void deleteProduct(){};
+    
+    public static void updateProduct(){};
+    
+    public static void populateDataset(){};
+}
