8907f1185f1374cd3a75fed0dc42e0e3176871fc
BlakeNorman12 <bnorman718@outlook.com>
2024-04-08 06:19:21 -0500

    added DeleteProduct() method to ProductMethods

--- src/methods/ProductMethods.java
+++ src/methods/ProductMethods.java
@@ -43,7 +43,26 @@ public class ProductMethods {
     };
     
     public static void deleteProduct(int itemID, String productName){
-    
+        SQLConnection connect = new SQLConnection();
+        
+        String sql = "DELETE FROM Employee WHERE ProductID = ? AND ProductName = ?";
+        
+        try (Connection conn = DriverManager.getConnection(connect.getURL(), connect.getUser(), connect.getPass());
+                PreparedStatement pstmt = conn.prepareStatement(sql)){
+            
+            pstmt.setInt(1, itemID);
+            pstmt.setString(2, productName);
+            
+            int affectedRows = pstmt.executeUpdate();
+            
+            if (affectedRows > 0){
+                System.out.println("Product deleted successfully");
+            } else {
+                System.out.println("No product found with the specified criteria");
+            }
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
     };
     
     public static void updateProduct(int itemID, String productName, int quantity){
