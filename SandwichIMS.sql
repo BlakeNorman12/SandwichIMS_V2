bf31e144b83c3fad0804d0314ce22186391b42ff
Jlevercom <124941702+Jlevercom@users.noreply.github.com>
2024-04-18 19:07:25 -0500

    Update SandwichIMS.sql
    Added test schema

--- SandwichIMS.sql
+++ SandwichIMS.sql
@@ -4,30 +4,22 @@ CREATE DATABASE SandwichIMS;
 
 USE SandwichIMS;
 
+
 CREATE TABLE Product (
 	ProductID INT AUTO_INCREMENT PRIMARY KEY,
     ProductName VARCHAR(50),
-    LastUpdated DATE,
+    LastUpdated DATETIME,
     Quantity INT,
-    UpdatedBy VARCHAR(50)
+    UpdatedBy VARCHAR(50),
+    ShelfLife VARCHAR(50)
 );
 
-INSERT INTO Product (ProductID, ProductName, LastUpdated, Quantity, UpdatedBy) VALUES
-(1, "Bread", '2024-04-08', 25, "Jordan Levercom"),
-(2, "Provolone", '2024-04-08', 50, "Blake Norman"),
-(3, "Mayo", '2024-04-08', 40, "Blake Norman"),
-(4, "Ham", '2024-04-08', 35, "Jordan Levercom"),
-(5, "Turkey", '2024-04-08', 20, "Jordan Levercom"),
-(6, "Avocado", '2024-04-08', 10, "Blake Norman"),
-(7, "Peppers", '2024-04-08', 28, "Blake Norman"),
-(8, "Cucumbers", '2024-04-08', 20, "Blake Norman"),
-(9, "Cheddar", '2024-04-08', 80, "Blake Norman");
 
 CREATE TABLE Employee (
 	employeeID INT AUTO_INCREMENT PRIMARY KEY,
     firstName VARCHAR(50),
     lastName VARCHAR(50),
-    username VARCHAR(50),
+    username VARCHAR(50) UNIQUE,
     password VARCHAR(255),
     isManager BOOLEAN
 );
@@ -38,3 +30,41 @@ INSERT INTO Employee (firstName, lastName, username, password, isManager) VALUES
 ('Derrick', 'Strover-Duncan', 'dstrover', '73567e977fbeb14cb66974484e2cf1ae7596b8744bdb7ddfd33457a0a44d5e44', TRUE),
 ('Elias', 'Gomez', 'egomez', '73567e977fbeb14cb66974484e2cf1ae7596b8744bdb7ddfd33457a0a44d5e44', TRUE);
 
+INSERT INTO Product (ProductName, LastUpdated, Quantity, UpdatedBy, ShelfLife) VALUES
+("White Bread", "2024-04-18", 0, "Jordan Levercom", "1 day"),
+("Wheat Bread", "2024-04-18", 0, "Blake Norman", "1 day"),
+("Cheddar Cheese", "2024-04-18", 0, "Derrick Strover-Duncan", "1 week"),
+("Turkey", "2024-04-18", 0, "Elias Gomez", "1 week");
+
+
+DROP DATABASE IF EXISTS TestSandwichIMS;
+
+CREATE DATABASE TestSandwichIMS;
+
+USE TestSandwichIMS;
+
+CREATE TABLE Product (
+	ProductID INT AUTO_INCREMENT PRIMARY KEY,
+    ProductName VARCHAR(50),
+    LastUpdated DATETIME,
+    Quantity INT,
+    UpdatedBy VARCHAR(50),
+    ShelfLife VARCHAR(50)
+);
+
+CREATE TABLE Employee (
+	employeeID INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(50),
+    lastName VARCHAR(50),
+    username VARCHAR(50) UNIQUE,
+    password VARCHAR(255),
+    isManager BOOLEAN
+);
+
+INSERT INTO Employee (firstName, lastName, username, password, isManager) VALUES
+('test', 'guy', 'testGuy', '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', TRUE),
+('add', 'test', 'testGuyNew', '6b86b273ff34fce19d6b804eff5a3f5747ada4eaa22f1d49c01e52ddb7875b4b', TRUE);
+
+INSERT INTO Product (ProductName, LastUpdated, Quantity, UpdatedBy, ShelfLife) VALUES
+("Test Product", "2024-04-18", 1, "Test Employee", ""),
+("Test Add Product", "2024-04-18", 1, "Test Employee", "Test Shelf Life");
